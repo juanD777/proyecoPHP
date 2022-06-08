c26f4ddaebfd5b2decb1c9aee5b0574130f5230b
juanD777 <104411088+juanD777@users.noreply.github.com>
2022-06-08 13:05:44 -0500

    Add files via upload

---
+++ saludos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Saludos P-H-P</title>
+</head>
+<body bgcolor="aqua">
+<!-- ?? -->
+<font color="blue">Ejecutando instruccion PHP</font> <br> </br>
+
+
+
+<p>
+<?php
+echo ("hola");
+?> </p>
+
+
+
+<p>
+PHP es un lenguaje de alto nivel que se ejecuta enen el servidor, es decir la ubicacion real o adstracta en donde se alojan las paginas del sitio
+</p>
+<p>
+Esto permite la visualisazion en cualquier navegador con esta estructura, se permite la progamacion gratuita.
+</p>
+<p>Si no contamos con un -servidor- no podemos ver funcionando el codigo PHP
+</p>
+
+
+
+</body>
+</html>
